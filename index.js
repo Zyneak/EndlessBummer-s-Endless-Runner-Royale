dc3f03cff1ef15d2ed3c850a8f398f74346321c2
Jordan Schnur <actjay1898@gmail.com>
2018-10-04 17:39:59 -0400

    Added Camera class

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 
 
-class Camera{
-  constructor(a) {
-      console.log(a);
-  }
-};
-
 
 const vsSource = `
-    attribute vec4 aVertexPosition;
+    attribute vec3 aVertexPosition;
 
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
 
+
+    varying vec3 v_positionWithOffset; 
     void main() {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        v_positionWithOffset = aVertexPosition;
+      gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition,1.0);
     }
   `;
 
-const fsSource = `
+const fsSource = `precision mediump float;
+varying vec3 v_positionWithOffset;
     void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        vec3 newVec = clamp(v_positionWithOffset,vec3(0.1,0.0,0.0),vec3(1,1,1));
+      gl_FragColor = vec4(newVec, 1.0);
     }
   `;
+
+class Camera{
+    constructor() {
+        this.position = vec3.fromValues(3,0,-4);
+        this.rotation = vec2.fromValues(0,0);
+        
+    }
+}
+
+
 
 class Game{
     constructor(canvas,setupCallback = this.setupCallback,draw) {
@@ -43,15 +52,49 @@ class Game{
 
         gl.bindBuffer(gl.ARRAY_BUFFER,this.positionBuffer);
         const positions = [
-            -1.0,  1.0,
-            1.0,  1.0,
-            -1.0, -1.0,
-            1.0, -1.0,
+            -1.0,-1.0,-1.0, // triangle 1 : begin
+            -1.0,-1.0, 1.0,
+            -1.0, 1.0, 1.0, // triangle 1 : end
+            1.0, 1.0,-1.0, // triangle 2 : begin
+            -1.0,-1.0,-1.0,
+            -1.0, 1.0,-1.0, // triangle 2 : end
+            1.0,-1.0, 1.0,
+            -1.0,-1.0,-1.0,
+            1.0,-1.0,-1.0,
+            1.0, 1.0,-1.0,
+            1.0,-1.0,-1.0,
+            -1.0,-1.0,-1.0,
+            -1.0,-1.0,-1.0,
+            -1.0, 1.0, 1.0,
+            -1.0, 1.0,-1.0,
+            1.0,-1.0, 1.0,
+            -1.0,-1.0, 1.0,
+            -1.0,-1.0,-1.0,
+            -1.0, 1.0, 1.0,
+            -1.0,-1.0, 1.0,
+            1.0,-1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0,-1.0,-1.0,
+            1.0, 1.0,-1.0,
+            1.0,-1.0,-1.0,
+            1.0, 1.0, 1.0,
+            1.0,-1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0,-1.0,
+            -1.0, 1.0,-1.0,
+            1.0, 1.0, 1.0,
+            -1.0, 1.0,-1.0,
+            -1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            -1.0, 1.0, 1.0,
+            1.0,-1.0, 1.0
         ];
         gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(positions),gl.STATIC_DRAW);
 
 
         this.draw = draw;
+        
+        
         //this.draw.call(this);
     }
 
@@ -111,12 +154,14 @@ class Game{
 
 
 
+
+let cam = new Camera();
 let a = new Game("quake_op_af",null,function(a) {
     let gl = this.glContext;
     gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
     gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);
+    gl.depthFunc(gl.LESS);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     //          Deg to Rad pi/180
@@ -129,11 +174,12 @@ let a = new Game("quake_op_af",null,function(a) {
     mat4.perspective(projectionMatrix,FOV,aspect,zNear,zFar);
 
     const modelViewMatrix = mat4.create();
-
-    mat4.translate(modelViewMatrix,modelViewMatrix,[-0,0,-6.0]);
+   // console.log(cam.rotation) 
+    mat4.rotateY(modelViewMatrix,modelViewMatrix,cam.rotation[0] * (Math.PI/180));
+    mat4.translate(modelViewMatrix,modelViewMatrix,[cam.position[0],0,cam.position[2]]);
 
     {
-        const numComponents = 2;
+        const numComponents = 3;
         const type = gl.FLOAT;
         const normalize = false;
         const stride = 0;
@@ -151,8 +197,77 @@ let a = new Game("quake_op_af",null,function(a) {
 
     {
         const offset  =0;
-        const vertextCount = 4;
-        gl.drawArrays(gl.TRIANGLE_STRIP,offset,vertextCount);
+        const vertextCount = 12*3;
+        gl.drawArrays(gl.TRIANGLES,offset,36);
+    }
+
+});
+
+a.canvas.addEventListener("click",function(e) {
+    e.target.requestPointerLock();
+})
+
+let speed = 0.01;
+document.addEventListener("keydown",function(e) {
+    console.log(cam);
+    let oldPos = this.position;
+    switch(e.key){
+        case "w":
+            
+            vec3.add(cam.position,
+                cam.position,
+               vec3.fromValues(
+                Math.cos(cam.rotation[0] * (Math.PI/180) )* speed,
+                0,
+                Math.sin(cam.rotation[0] * (Math.PI/180) )* speed));
+                console.log(cam.position);
+                console.log( (Math.cos(cam.rotation[0] * (Math.PI/180) )* speed) + " " +  Math.sin(cam.rotation[0] * (Math.PI/180) )* speed)
+            break;
+            case "s":
+            
+            vec3.add(cam.position,
+                cam.position,
+               vec3.fromValues(
+                Math.cos((cam.rotation[0]+180) * (Math.PI/180))* speed,
+                0,
+                Math.sin((cam.rotation[0]+180) * (Math.PI/180))* speed));
+            console.log(cam.position);
+            break;
+            case "a":
+            
+            vec3.add(cam.position,
+                cam.position,
+               vec3.fromValues(
+                Math.cos((cam.rotation[0]+90) * (Math.PI/180) )* speed,
+                0,
+                Math.sin((cam.rotation[0]+90) * (Math.PI/180) )* speed));
+            console.log(cam.position);
+            break;
+            case "d":
+            
+            vec3.add(cam.position,
+                cam.position,
+               vec3.fromValues(
+                Math.cos((cam.rotation[0]-90) * (Math.PI/180))* speed,
+                0,
+                Math.sin((cam.rotation[0]-90) * (Math.PI/180))* speed));
+            console.log(cam.position);
+            break;
+
+            case "q":
+            
+            vec2.add(cam.rotation,
+                cam.rotation,
+               vec2.fromValues(-1,0));
+            console.log("Rot: " + cam.rotation);
+            break;
+            case "e":
+            
+            vec2.add(cam.rotation,
+                cam.rotation,
+               vec2.fromValues(1,0));
+            console.log("Rot: " + cam.rotation);
+            break;
     }
 
 });
